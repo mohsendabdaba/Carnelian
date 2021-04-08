0a3aa8e5eef0edd4b766dadb9b0fc2125903c95f
unknown <chifaaachelly@gmail.com>
2021-04-08 13:45:50 +0100

    added CLOs list

--- src/components/CLOCard.js
+++ src/components/CLOCard.js
@@ -5,74 +5,67 @@ import {
     CardContent,
     Grid,
     Typography
-  } from '@material-ui/core';
-  import { green } from '@material-ui/core/colors';
-  // import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-  import PeopleIcon from '@material-ui/icons/PeopleOutlined';
-  
-  const TotalCustomers = (props) => (
-    <Card {...props}>
-      <CardContent>
-        <Grid
-          container
-          spacing={3}
-          sx={{ justifyContent: 'space-between' }}
-        >
-          <Grid item>
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="h3"
-            
-  
-            >
-              CLOS
-            </Typography>
-            {/* <Typography
-              color="textPrimary"
-              variant="h3"
-            >
-              30 students subscribed
-            </Typography> */}
-          </Grid>
-          <Grid item>
-            <Avatar
-              sx={{
-                backgroundColor: green[600],
-                height: 56,
-                width: 56
-              }}
-            >
-              <PeopleIcon />
-            </Avatar>
-          </Grid>
-        </Grid>
-        <Box
-          sx={{
-            alignItems: 'center',
-            display: 'flex',
-            pt: 2
-          }}
-        >
-          {/* <ArrowUpwardIcon sx={{ color: green[900] }} />
-          <Typography
-            variant="body2"
-            sx={{
-              color: green[900],
-              mr: 1
-            }}
-          >
-            16%
-          </Typography> */}
-          <Typography
-            variant="h5"
-          >
-            30 students subscribed
-          </Typography>
-        </Box>
-      </CardContent>
-    </Card>
-  );
-  
-  export default TotalCustomers;
-  
+} from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+// import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import PeopleIcon from '@material-ui/icons/PeopleOutlined';
+
+const TotalCustomers = (props) => {
+
+    console.log(props.outcome, 'here it is')
+    return (
+        <Card {...props}>
+            <CardContent>
+                <Grid
+                    container
+                    spacing={3}
+                    sx={{ justifyContent: 'space-between' }}
+                >
+                    <Grid item>
+                        <Typography
+                            color="textSecondary"
+                            gutterBottom
+                            variant="h3"
+
+                        >
+                            {props.outcome.title}
+                        </Typography>
+                    </Grid>
+                  
+                    <Grid item>
+                        <Avatar
+                            sx={{
+                                backgroundColor: green[600],
+                                height: 56,
+                                width: 56
+                            }}
+                        >
+                            <PeopleIcon />
+                        </Avatar>
+                    </Grid>
+                </Grid>
+
+               
+                <Box
+                    sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        pt: 2
+                    }}
+                >
+                    <Grid item>
+                        <Typography
+                            color="textSecondary"
+                            gutterBottom
+                            variant="h5"
+                        >
+                            {props.outcome.Domain}
+                        </Typography>
+                    </Grid>
+                </Box>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default TotalCustomers;
--- src/pages/Outcomes.js
+++ src/pages/Outcomes.js
@@ -25,7 +25,7 @@
 //             <Helmet>
 //                 <title> Assessments | Material Kit</title>
 //             </Helmet>
-          
+
 //         </Box>
 //     )
 // }
@@ -42,13 +42,11 @@ import {
 import courses from '../__mocks__/courses.json'
 import DashboardSidebar from '../components/DashboardSidebar';
 import { useState } from 'react';
-import CLOCard from'../components/CLOCard';
+import CLOCard from '../components/CLOCard';
 
 const Outcomes = () => {
     const [isMobileNavOpen, setMobileNavOpen] = useState(false);
-    const filteredItems = courses.filter(item => item.id=="001")
-
-    // let CourseLearningOutcomes=CourseLearningOutcomes(filteredItems.CourseLearningOutcomes)
+    const filteredItems = courses.filter(item => item.id == "001")
     console.log(courses, "courses")
     console.log(filteredItems, "outcomes")
     console.log(filteredItems[0].courseLearningOutcomes, "CourseLearningOutcomes")
@@ -77,24 +75,24 @@ const Outcomes = () => {
             >
                 <h2>Junior Design Seminar</h2>
             </Grid>
-            
+
             <Box sx={{ pt: 3 }}>
-          <Grid
-            container
-            spacing={3}
-          >
-            {filteredItems[0].courseLearningOutcomes.map((outcome) => (
-              <Grid
-                item
-                key={outcome.id}
-                lg={12}
-                md={12}
-                xs={12}
-              >
-<CLOCard/>              </Grid>
-            ))}
-          </Grid>
-        </Box>
+                <Grid
+                    container
+                    spacing={3}
+                >
+                    {filteredItems[0].courseLearningOutcomes.map(outcome => (
+                        <Grid
+                            item
+                            key={outcome.id}
+                            lg={12}
+                            md={12}
+                            xs={12}
+                        >
+                            <CLOCard outcome={outcome}/>              </Grid>
+                    ))}
+                </Grid>
+            </Box>
         </Box>
     )
 }
